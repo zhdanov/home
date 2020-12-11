a429eac02ec232722f16befc539586008c31957c
zhdanov <yuriy.zhdanov@gmail.com>
2020-12-11 16:42:35 +0300

    feat(setup): ncdu, htop

--- setup/setup__packages.bash
+++ setup/setup__packages.bash
@@ -4,4 +4,4 @@ sudo apt clean
 sudo apt -y update
 sudo apt -y upgrade
 
-sudo apt -y install vim git openssh-server nodejs npm tmux curl
+sudo apt -y install vim git openssh-server nodejs npm tmux curl ncdu htop
