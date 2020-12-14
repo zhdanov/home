84dfad3bafbb045d97ea06650dc3a9596a5cd9c5
zhdanov <yuriy.zhdanov@gmail.com>
2020-12-14 16:10:20 +0300

    docs(setup): yandex-disk optional notice

--- setup/setup__yandex-disk.bash
+++ setup/setup__yandex-disk.bash
@@ -2,6 +2,16 @@
 
 if [[ ! -f "/etc/apt/sources.list.d/yandex-disk.list" ]]; then
     echo "deb http://repo.yandex.ru/yandex-disk/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/yandex-disk.list > /dev/null && wget http://repo.yandex.ru/yandex-disk/YANDEX-DISK-KEY.GPG -O- | sudo apt-key add - && sudo apt-get update && sudo apt-get install -y yandex-disk
-    echo "==== Yandex Disk ===="
+
+    cat << EOF
+    ==== Yandex Disk ====
+    == optional ==
+vim $HOME/.config/yandex-disk/config.cfg
+exclude-dirs=home,tmp
+read-only="true"
+overwrite="true"
+    == /optional ==
+EOF
+
     yandex-disk setup
 fi
