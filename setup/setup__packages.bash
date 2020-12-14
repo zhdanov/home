d98403b3120b43a3204da2ba081625b374500feb
zhdanov <yuriy.zhdanov@gmail.com>
2020-12-14 22:48:56 +0300

    feat(setup): fzf + rg

--- setup/setup__packages.bash
+++ setup/setup__packages.bash
@@ -4,4 +4,4 @@ sudo apt clean
 sudo apt -y update
 sudo apt -y upgrade
 
-sudo apt -y install vim git openssh-server net-tools nodejs npm tmux curl ncdu htop
+sudo apt -y install vim git openssh-server net-tools nodejs npm tmux curl ncdu htop fzf ripgrep
