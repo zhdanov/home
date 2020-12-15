56337ca5b87bd787260559165b35afbfbd0b5a64
zhdanov <yuriy.zhdanov@gmail.com>
2020-12-15 17:45:35 +0300

    fix(setup): skip hold error message

--- setup/setup__kubernetes.bash
+++ setup/setup__kubernetes.bash
@@ -6,7 +6,9 @@ cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 sudo apt update
+set +e
 sudo apt install -y docker.io kubectl docker-compose parallel
+set -e
 sudo apt-mark hold docker.io kubectl docker-compose
 
 sudo usermod -aG docker $USER
