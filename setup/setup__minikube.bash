d02c5f0e084279a017ede1d91a8cd36a2d59982f
zhdanov <yuriy.zhdanov@gmail.com>
2020-12-21 20:04:39 +0300

    feat(setup): nfs instead of minikube mount

--- setup/setup__minikube.bash
+++ setup/setup__minikube.bash
@@ -11,17 +11,15 @@ if [ $(minikube status | grep Running | wc -l) -gt 0 ]; then
     echo "minikube is already started"
 elif docker ps -a | grep -q minikube; then
     echo "starting existing minikube"
-    pkill -f "minikube mount $HOME/data-store"
 
     minikube start
+
+    # registry
     export REGISTRY_IP=$(kubectl -n kube-system get service registry -o=template={{.spec.clusterIP}})
     minikube ssh "cat /etc/hosts | grep -v werf-registry | sudo tee /etc/hosts"
     minikube ssh "echo '$REGISTRY_IP $HOME_REGISTRY' | sudo tee -a /etc/hosts"
 
     kubectl -n kube-system wait --for=condition=ready --timeout=120s pods -l app.kubernetes.io/component=controller
-
-    nohup minikube mount $HOME/data-store:/data-store &
-
 else
     eval $(ssh-agent -s)
     ssh-add
@@ -69,7 +67,34 @@ EOF
     sudo sed -i -e '/^.*werf-registry.*$/d' /etc/hosts
     echo `minikube ip`" $HOME_REGISTRY" | sudo tee -a /etc/hosts
 
-    nohup minikube mount $HOME/data-store:/data-store &
+    # nfs host
+    sudo sed -i -e '/^.*data-store.*$/d' /etc/exports
+    echo "$HOME/data-store "`minikube ip`"(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
+
+    sudo systemctl restart nfs-kernel-server
+
+    cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: nfs-pv-data-store
+spec:
+  storageClassName: manual
+  capacity:
+    storage: 200Gi
+  accessModes:
+    - ReadWriteMany
+  persistentVolumeReclaimPolicy: Retain
+  nfs:
+    server: host.minikube.internal
+    path: $HOME/data-store
+EOF
+
+    # nfs client (example)
+    #minikube ssh "sudo mkdir /data-store"
+    #minikube ssh "sudo mount host.minikube.internal:$HOME/data-store /data-store"
+    #minikube ssh "cat /etc/fstab | grep -v data-store | sudo tee /etc/fstab"
+    #minikube ssh "echo 'host.minikube.internal:$HOME/data-store    /nfs/home    nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0' | sudo tee -a /etc/fstab"
 
     kubectl -n kube-system wait --for=condition=ready --timeout=120s pods -l app.kubernetes.io/component=controller
 fi
