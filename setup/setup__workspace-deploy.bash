144247195662c5d5b5237de9ce6d3dc729ac7894
zhdanov <yuriy.zhdanov@gmail.com>
2020-12-28 20:34:11 +0300

    feat(setup): smart deploy

--- setup/setup__workspace-deploy.bash
+++ setup/setup__workspace-deploy.bash
@@ -1,29 +1,26 @@
 #!/bin/bash
+
 . $(multiwerf use 1.1 stable --as-file)
 
 . setup_def.bash
 
-# begin to automate
-ENV=prod
-TAG=0.3
-pushd $HOME/workspace/production/skeleton
-APPNAME=skeleton
-# end to automate
-
 kubectl config use-context $HOME_KUBECONTEXT
 
-werf build-and-publish --kube-config=$HOME_KUBECONFIG --stages-storage :local \
---images-repo-implementation='harbor' \
---insecure-registry=true \
---skip-tls-verify-registry=true \
--i=$HOME_REGISTRY/$APPNAME \
---tag-custom $TAG || echo "local" > /dev/null
+TAG=0.1
+for namespace in `ls $HOME/workspace`; do
+    for appname in `ls $HOME/workspace/$namespace`; do
+        pushd $HOME/workspace/$namespace/$appname
+            werf build-and-publish --kube-config=$HOME_KUBECONFIG --stages-storage :local \
+            --images-repo-implementation='harbor' \
+            --insecure-registry=true \
+            --skip-tls-verify-registry=true \
+            -i=$HOME_REGISTRY/$appname \
+            --tag-custom $TAG || echo "local" > /dev/null
 
-werf deploy --kube-config=$HOME_KUBECONFIG --env=$ENV --stages-storage :local \
---images-repo-implementation='harbor' --insecure-registry=true \
---skip-tls-verify-registry=true -i=$HOME_REGISTRY/$APPNAME \
---tag-custom $TAG || echo "local" > /dev/null
-
-# begin to automate
-popd
-# end to automate
+            werf deploy --kube-config=$HOME_KUBECONFIG --env=$namespace --stages-storage :local \
+            --images-repo-implementation='harbor' --insecure-registry=true \
+            --skip-tls-verify-registry=true -i=$HOME_REGISTRY/$appname \
+            --tag-custom $TAG || echo "local" > /dev/null
+        popd
+    done
+done
