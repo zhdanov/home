83b25815f6c8f5c508a1e5433c0d10118784de04
zhdanov <yuriy.zhdanov@gmail.com>
2020-12-29 18:05:47 +0300

    feat(setup): tree package

--- setup/setup__packages.bash
+++ setup/setup__packages.bash
@@ -4,4 +4,4 @@ sudo apt clean
 sudo apt -y update
 sudo apt -y upgrade
 
-sudo apt -y install vim git openssh-server net-tools curl nfs-kernel-server nodejs npm i3 tmux vim htop fzf ripgrep ncdu
+sudo apt -y install vim git openssh-server net-tools curl tree nfs-kernel-server nodejs npm i3 tmux vim htop fzf ripgrep ncdu
