6fec472dab7e6ab2dcd4c235f5cf53faec433670
zhdanov <yuriy.zhdanov@gmail.com>
2021-01-04 01:09:54 +0300

    feat(backup): cloud drive list

--- bin/backup.bash
+++ bin/backup.bash
@@ -1,11 +1,18 @@
 #!/bin/bash
 #
-# 1. Make file workspace/%namespace%/backup-list.txt
-# 2. Cron by root:
-# 30 4 * * * /home/%user%/bin/backup.bash
+# 1. Execute ./setup/setup__cloud-drive.bash
+# 2. Make file workspace/%namespace%/backup-list.txt
+#    with list dirs for backup
+# 3. Cron by root:
+#    30 4 * * * /home/%user%/bin/backup.bash
 #
 
 cd "$(dirname "$0")"
+
+CLOUD_DIR_LIST=(
+Yandex.Disk
+Dropbox
+)
 
 NOW=$(date +"%Y-%m-%d")
 NOW_D=$(date +"%d")
@@ -13,32 +20,37 @@ NOW_MD=$(date +"%m-%d")
 
 for namespace in `ls ../data-store`; do
     if [[ -f "../data-store/$namespace/backup-list.txt" ]]; then
-        if [[ ! -f "../Yandex.Disk/backup/daily/$NOW/$namespace" ]]; then
-            mkdir -p ../Yandex.Disk/backup/daily/$NOW/$namespace
-        fi
 
-        cp ../data-store/$namespace/backup-list.txt ../Yandex.Disk/backup/daily/$NOW/$namespace/
-
-        for item in `cat ../data-store/$namespace/backup-list.txt`; do
-            pushd ../data-store/$namespace
-                zip -r $item.zip $item
-            popd
-
-            if [[ $NOW_D -eq "02" ]]; then
-                if [[ ! -f "../Yandex.Disk/backup/monthly/$NOW/$namespace" ]]; then
-                    mkdir -p ../Yandex.Disk/backup/monthly/$NOW/$namespace
-                fi
-                cp ../data-store/$namespace/$item.zip ../Yandex.Disk/backup/monthly/$NOW/$namespace/
+        for clouddir in "${CLOUD_DIR_LIST[@]}"
+        do
+            if [[ ! -d "../$clouddir/backup/daily/$NOW/$namespace" ]]; then
+                mkdir -p ../$clouddir/backup/daily/$NOW/$namespace
             fi
 
-            if [[ $NOW_MD -eq "01-02" ]]; then
-                if [[ ! -f "../Yandex.Disk/backup/yearly/$NOW/$namespace" ]]; then
-                    mkdir -p ../Yandex.Disk/backup/yearly/$NOW/$namespace
-                fi
-                cp ../data-store/$namespace/$item.zip ../Yandex.Disk/backup/yearly/$NOW/$namespace/
-            fi
+            cp ../data-store/$namespace/backup-list.txt ../$clouddir/backup/daily/$NOW/$namespace/
 
-            mv ../data-store/$namespace/$item.zip ../Yandex.Disk/backup/daily/$NOW/$namespace/
+            for item in `cat ../data-store/$namespace/backup-list.txt`; do
+                pushd ../data-store/$namespace
+                    zip -r $item.zip $item
+                popd
+
+                if [[ $NOW_D -eq "02" ]]; then
+                    if [[ ! -f "../$clouddir/backup/monthly/$NOW/$namespace" ]]; then
+                        mkdir -p ../$clouddir/backup/monthly/$NOW/$namespace
+                    fi
+                    cp ../data-store/$namespace/$item.zip ../$clouddir/backup/monthly/$NOW/$namespace/
+                fi
+
+                if [[ $NOW_MD -eq "01-02" ]]; then
+                    if [[ ! -f "../$clouddir/backup/yearly/$NOW/$namespace" ]]; then
+                        mkdir -p ../$clouddir/backup/yearly/$NOW/$namespace
+                    fi
+                    cp ../data-store/$namespace/$item.zip ../$clouddir/backup/yearly/$NOW/$namespace/
+                fi
+
+                mv ../data-store/$namespace/$item.zip ../$clouddir/backup/daily/$NOW/$namespace/
+            done
         done
+
     fi
 done
