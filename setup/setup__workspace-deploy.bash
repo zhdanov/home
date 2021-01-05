3912149216ec639d40000752cc0bbae12b1dc557
zhdanov <yuriy.zhdanov@gmail.com>
2021-01-06 02:28:02 +0300

    feat(deploy): single app deploy, support pre/post deploy scripts

--- setup/setup__workspace-deploy.bash
+++ setup/setup__workspace-deploy.bash
@@ -10,20 +10,42 @@ kubectl config use-context $HOME_KUBECONTEXT
 
 TAG=`echo -n "$(date)" | md5sum | awk '{print $1}'`
 
-for namespace in `ls $HOME/workspace`; do
-    for appname in `ls $HOME/workspace/$namespace`; do
-        pushd $HOME/workspace/$namespace/$appname
-            werf build-and-publish --kube-config=$HOME_KUBECONFIG --stages-storage :local \
-            --images-repo-implementation='harbor' \
-            --insecure-registry=true \
-            --skip-tls-verify-registry=true \
-            -i=$HOME_REGISTRY/$appname \
-            --tag-custom $TAG || echo "local" > /dev/null
 
-            werf deploy --kube-config=$HOME_KUBECONFIG --env=$namespace --stages-storage :local \
-            --images-repo-implementation='harbor' --insecure-registry=true \
-            --skip-tls-verify-registry=true -i=$HOME_REGISTRY/$appname \
-            --tag-custom $TAG || echo "local" > /dev/null
-        popd
+function deploy()
+{
+    namespace=$1
+    appname=$2
+
+    pushd $HOME/workspace/$namespace/$appname
+        if [[ -f "./.helm/predeploy.bash" ]]; then
+            ./.helm/predeploy.bash
+        fi
+
+        werf build-and-publish --kube-config=$HOME_KUBECONFIG --stages-storage :local \
+        --images-repo-implementation='harbor' \
+        --insecure-registry=true \
+        --skip-tls-verify-registry=true \
+        -i=$HOME_REGISTRY/$appname \
+        --tag-custom $TAG || echo "local" > /dev/null
+
+        werf deploy --kube-config=$HOME_KUBECONFIG --env=$namespace --stages-storage :local \
+        --images-repo-implementation='harbor' --insecure-registry=true \
+        --skip-tls-verify-registry=true -i=$HOME_REGISTRY/$appname \
+        --tag-custom $TAG || echo "local" > /dev/null
+
+        if [[ -f "./.helm/postdeploy.bash" ]]; then
+            ./.helm/postdeploy.bash
+        fi
+    popd
+}
+
+
+if [[ $# -eq 2 ]]; then
+    deploy $1 $2
+else
+    for namespace in `ls $HOME/workspace`; do
+        for appname in `ls $HOME/workspace/$namespace`; do
+            deploy $namespace $appname
+        done
     done
-done
+fi
