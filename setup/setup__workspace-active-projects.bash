5de804a15c88c402e6be4c8ec9bc47a4236bfc15
zhdanov <yuriy.zhdanov@gmail.com>
2021-01-10 02:39:44 +0300

    feat(setup): develop directory

--- setup/setup__workspace-active-projects.bash
+++ setup/setup__workspace-active-projects.bash
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+if [[ ! -d "$HOME/develop" ]]; then
+    mkdir $HOME/develop
+fi
+
 if [[ -d "$HOME/Yandex.Disk/git-store" ]]; then
     for repo in `ls $HOME/Yandex.Disk/git-store`; do
         if [[ -f "$HOME/Yandex.Disk/git-store/$repo/namespace-list.txt" ]]; then
             for namespace in `cat $HOME/Yandex.Disk/git-store/$repo/namespace-list.txt`; do
                 if [[ ! -h "$HOME/workspace/$namespace/$repo" ]]; then
                     ln -s $HOME/Yandex.Disk/git-store/$repo $HOME/workspace/$namespace/$repo
+                fi
+
+                if [[ ! -h "$HOME/develop/$repo" ]]; then
+                    ln -s $HOME/Yandex.Disk/git-store/$repo $HOME/develop/$repo
                 fi
             done
         fi
