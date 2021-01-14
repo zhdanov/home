c61869540b61151e517f62f2cc5ff6fb4c82b08e
zhdanov <yuriy.zhdanov@gmail.com>
2021-01-14 23:20:35 +0300

    feat(setup): ssh configure

--- setup/setup__configure-ssh.bash
+++ setup/setup__configure-ssh.bash
@@ -5,6 +5,18 @@ if [[ $SETUP_TYPE == "master" ]]; then
         echo "changed ssh port to $HOME_SSH_PORT in /etc/ssh/sshd_config"
         sudo sed -i "/.*#Port 22.*/c\Port $HOME_SSH_PORT" /etc/ssh/sshd_config
     fi
+    if grep -q "#ClientAliveInterval" /etc/ssh/sshd_config; then
+        echo "changed ClientAliveInterval in /etc/ssh/sshd_config"
+        sudo sed -i "/.*#ClientAliveInterval.*/c\ClientAliveInterval 30" /etc/ssh/sshd_config
+    fi
+    if grep -q "#TCPKeepAlive" /etc/ssh/sshd_config; then
+        echo "changed TCPKeepAlive in /etc/ssh/sshd_config"
+        sudo sed -i "/.*#TCPKeepAlive.*/c\TCPKeepAlive yes" /etc/ssh/sshd_config
+    fi
+    if grep -q "#ClientAliveCountMax" /etc/ssh/sshd_config; then
+        echo "changed ClientAliveCountMax in /etc/ssh/sshd_config"
+        sudo sed -i "/.*#ClientAliveCountMax.*/c\ClientAliveCountMax 99999" /etc/ssh/sshd_config
+    fi
 fi
 
 if grep -q "#PermitRootLogin" /etc/ssh/sshd_config; then
