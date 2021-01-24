bce5993ebcafff80aeb91a1ab22019e62136d744
zhdanov <yuriy.zhdanov@gmail.com>
2021-01-24 22:23:31 +0300

    refactor(gitlab): backup script improve

--- workspace/production/gitlab/backup.bash
+++ workspace/production/gitlab/backup.bash
@@ -7,11 +7,12 @@ if [[ $# -ne 1 ]]; then
 fi
 
 HOME_USER_NAME=$1
+SERVICE_NAME=gitlab-prod
 
-if kubectl -n gitlab-prod get statefulsets.apps | grep -q gitlab-prod; then
+if kubectl -n $SERVICE_NAME get statefulsets.apps | grep -q $SERVICE_NAME; then
     if [[ $(dirname "$0") =~ (.*)/(.*)/(.*)$ ]]; then
         BACKUP_FILE_NAME="${BASH_REMATCH[2]}-${BASH_REMATCH[3]}-backup.tar"
-        kubectl -n gitlab-prod exec -it gitlab-prod-0 -c gitlab-ce -- gitlab-backup create BACKUP=prod CRON=1
-        mv /home/$HOME_USER_NAME/data-store/gitlab-prod/gitlab-data/backups/prod_gitlab_backup.tar /home/$HOME_USER_NAME/data-store/gitlab-prod/$BACKUP_FILE_NAME
+        kubectl -n $SERVICE_NAME exec -it $SERVICE_NAME-0 -c gitlab-ce -- gitlab-backup create BACKUP=prod CRON=1
+        mv /home/$HOME_USER_NAME/data-store/$SERVICE_NAME/gitlab-data/backups/prod_gitlab_backup.tar /home/$HOME_USER_NAME/data-store/$SERVICE_NAME/$BACKUP_FILE_NAME
     fi
 fi
