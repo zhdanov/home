99ba4cdf8bc88c88c739399584fa38bebc5c27c1
zhdanov <yuriy.zhdanov@gmail.com>
2022-05-02 22:00:17 +0300

    fix(commitizen): install

--- setup/setup__commitizen.bash
+++ setup/setup__commitizen.bash
@@ -2,6 +2,6 @@
 set -eux
 
 pushd "$(dirname "$0")"
-    . setup__nodejs.bash
-    sudo npm install commitizen -g
+    npm install commitizen -g
+    npm install -g commitizen cz-conventional-changelog && echo '{ "path": "cz-conventional-changelog" }' > ~/.czrc
 popd
--- setup/setup__nodejs.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-HOME_NODEJS_VERSION=14.15.1
-
-sudo npm i -g npm n
-
-if [[ $(node --version) != "v$HOME_NODEJS_VERSION" ]]; then
-    sudo n $HOME_NODEJS_VERSION
-fi
