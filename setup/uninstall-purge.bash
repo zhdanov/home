5657618ac03666594a1cef7362ab5f52a3723373
zhdanov <yuriy.zhdanov@gmail.com>
2022-07-14 19:40:04 +0300

    fix(uninstall): purge

--- setup/uninstall-purge.bash
+++ setup/uninstall-purge.bash
@@ -1,19 +1,7 @@
 #!/bin/bash
 pushd "$(dirname "$0")"
 
-    . $(multiwerf use 1.1 stable --as-file)
-
     . setup_def.bash
-
-    for environment in `ls $HOME/workspace`; do
-        for appname in `ls $HOME/workspace/$environment`; do
-            pushd $HOME/workspace/$environment/$appname
-                export environment=$environment
-                export appname=$appname
-                werf stages purge --force --stages-storage=:local
-            popd
-        done
-    done
 
     werf host cleanup
     werf host purge
