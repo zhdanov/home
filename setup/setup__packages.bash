a7f6e74d09f1d966ebd54167c23979e1551f5ec4
zhdanov <yuriy.zhdanov@gmail.com>
2022-10-27 08:21:45 +0300

    fix(i3wm): screenshots

--- setup/setup__packages.bash
+++ setup/setup__packages.bash
@@ -4,7 +4,7 @@ sudo apt clean
 sudo apt -y update
 sudo apt -y upgrade
 
-sudo apt -y install vim git openssh-server net-tools curl hdparm tree nfs-kernel-server ffmpeg unrar nodejs npm tmux vim htop atop smem fzf ripgrep ncdu
+sudo apt -y install vim git openssh-server net-tools curl maim hdparm tree nfs-kernel-server ffmpeg unrar nodejs npm tmux vim htop atop smem fzf ripgrep ncdu
 sudo apt -y install i3 i3status nemo ttf-mscorefonts-installer kate fbreader audacity growisofs peek screenkey cheese brightnessctl tcptrack
 
 . setup__google-chrome.bash
