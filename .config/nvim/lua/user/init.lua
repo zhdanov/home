f8b5a0a8d81d8f594874e0a2d65fb8f16623034a
zhdanov <yuriy.zhdanov@gmail.com>
2023-03-19 20:54:09 +0300

    feat(dotfiles): update astronvim

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -219,7 +219,7 @@ local config = {
 
   -- Configure plugins
   plugins = {
-    init = {
+    --init = {
       -- You can disable default plugins as follows:
       -- ["goolord/alpha-nvim"] = { disable = true },
 
@@ -240,36 +240,36 @@ local config = {
       --     require("lsp_signature").setup()
       --   end,
       -- },
-    },
+    --},
     -- All other entries override the require("<key>").setup({...}) call for default plugins
-    ["null-ls"] = function(config) -- overrides `require("null-ls").setup(config)`
+    --["null-ls"] = function(config) -- overrides `require("null-ls").setup(config)`
       -- config variable is the default configuration table for the setup function call
       -- local null_ls = require "null-ls"
 
       -- Check supported formatters and linters
       -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
       -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-      config.sources = {
+    --  config.sources = {
         -- Set a formatter
         -- null_ls.builtins.formatting.stylua,
         -- null_ls.builtins.formatting.prettier,
-      }
-      return config -- return final config table
-    end,
-    treesitter = { -- overrides `require("treesitter").setup(...)`
+    --  }
+    --  return config -- return final config table
+    --end,
+    --treesitter = { -- overrides `require("treesitter").setup(...)`
       -- ensure_installed = { "lua" },
-    },
+    --},
     -- use mason-lspconfig to configure LSP installations
-    ["mason-lspconfig"] = { -- overrides `require("mason-lspconfig").setup(...)`
+    --["mason-lspconfig"] = { -- overrides `require("mason-lspconfig").setup(...)`
       -- ensure_installed = { "sumneko_lua" },
-    },
+    --},
     -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
-    ["mason-null-ls"] = { -- overrides `require("mason-null-ls").setup(...)`
+    --["mason-null-ls"] = { -- overrides `require("mason-null-ls").setup(...)`
       -- ensure_installed = { "prettier", "stylua" },
-    },
-    ["mason-nvim-dap"] = { -- overrides `require("mason-nvim-dap").setup(...)`
+    --},
+    --["mason-nvim-dap"] = { -- overrides `require("mason-nvim-dap").setup(...)`
       -- ensure_installed = { "python" },
-    },
+    --},
   },
 
   -- LuaSnip Options
