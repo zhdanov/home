9c7915f5b2a1bb30ad854309b072cc27ad3828da
zhdanov <yuriy.zhdanov@gmail.com>
2023-03-20 22:09:37 +0300

    fix(dotfiles): astronvim autoopen neotree

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -81,5 +81,15 @@ return {
     --     ["~/%.config/foo/.*"] = "fooscript",
     --   },
     -- }
+    vim.api.nvim_create_augroup("neotree", {})
+    vim.api.nvim_create_autocmd("UiEnter", {
+      desc = "Open Neotree automatically",
+      group = "neotree",
+      callback = function()
+        if vim.fn.argc() == 0 then
+          vim.cmd "Neotree toggle"
+        end
+      end,
+    })
   end,
 }
