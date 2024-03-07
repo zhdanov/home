94dd7bdd1a831c5f476a52cffa858845fb57067d
zhdanov <yuriy.zhdanov@gmail.com>
2024-03-07 05:44:55 +0300

    fix(setup): update dropbox

--- setup/setup__update-dropbox.bash
+++ setup/setup__update-dropbox.bash
@@ -3,7 +3,7 @@ pushd "$(dirname "$0")"
 
     [ ! -d /opt/dropbox ] && mkdir /opt/dropbox/
 
-    wget https://www.dropbox.com/download?plat=lnx.x86_64 -O dropbox-linux.tar.gz
+    wget --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3" -O dropbox-linux.tar.gz https://www.dropbox.com/download?plat=lnx.x86_64
 
     if [ -f dropbox-linux.tar.gz ]
     then
