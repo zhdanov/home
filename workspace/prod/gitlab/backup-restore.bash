733ff306d92fd0977155db3bf8a4f88110cb4c45
zhdanov <yuriy.zhdanov@gmail.com>
2024-08-04 15:09:20 +0000

    fix(setup): restore gitlab

--- workspace/prod/gitlab/backup-restore.bash
+++ workspace/prod/gitlab/backup-restore.bash
@@ -7,16 +7,16 @@
 BACKUP_NAME=prod
 
 if kubectl -n gitlab-prod get statefulsets.apps | grep -q gitlab-prod; then
-    kubectl -n gitlab-prod exec -it gitlab-prod-0 -c gitlab -- gitlab-ctl stop unicorn
-    kubectl -n gitlab-prod exec -it gitlab-prod-0 -c gitlab -- gitlab-ctl stop puma
-    kubectl -n gitlab-prod exec -it gitlab-prod-0 -c gitlab -- gitlab-ctl stop sidekiq
+    kubectl -n gitlab-prod exec -it gitlab-prod-0 -c gitlab-ce -- gitlab-ctl stop unicorn
+    kubectl -n gitlab-prod exec -it gitlab-prod-0 -c gitlab-ce -- gitlab-ctl stop puma
+    kubectl -n gitlab-prod exec -it gitlab-prod-0 -c gitlab-ce -- gitlab-ctl stop sidekiq
 
     sleep 5
 
-    kubectl -n gitlab-prod exec -it gitlab-prod-0 -c gitlab -- chown -R git:root /var/opt/gitlab/backups
-    kubectl -n gitlab-prod exec -it gitlab-prod-0 -c gitlab -- gitlab-backup restore BACKUP=$BACKUP_NAME
+    kubectl -n gitlab-prod exec -it gitlab-prod-0 -c gitlab-ce -- chown -R git:root /var/opt/gitlab/backups
+    kubectl -n gitlab-prod exec -it gitlab-prod-0 -c gitlab-ce -- gitlab-backup restore BACKUP=$BACKUP_NAME
 
     sleep 5
- 
-    kubectl -n gitlab-prod exec -it gitlab-prod-0 -c gitlab -- /sbin/killall5
+
+    kubectl -n gitlab-prod exec -it gitlab-prod-0 -c gitlab-ce -- /sbin/killall5
 fi
