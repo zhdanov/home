078049a2348deee1b6cd138fc09caea83c627161
zhdanov <george@jupiter.solutions>
2024-09-26 19:51:49 +0300

    feat(backup): zip to tar

--- bin/backup-git-store.bash
+++ bin/backup-git-store.bash
@@ -14,19 +14,19 @@ Dropbox
 # backup bare
 HOME_USER_NAME=$1
 pushd /home/$HOME_USER_NAME/
-#    zip -r git-store.zip git-store
-#    mv git-store.zip data-store/gitlab-prod/
+    tar -cvf git-store.tar git-store
+    mv git-store.tar data-store/gitlab-prod/
 popd
 
 # backup cloned
 mkdir -p /tmp/backup-git-store
 pushd /tmp/backup-git-store
     for repo in `ls /home/$HOME_USER_NAME/git-store`; do git clone /home/$HOME_USER_NAME/git-store/$repo; done
-    for repo in `ls /home/$HOME_USER_NAME/git-store`; do repo_dir="${repo::-4}" && zip -r $repo_dir.zip $repo_dir; done
+    for repo in `ls /home/$HOME_USER_NAME/git-store`; do repo_dir="${repo::-4}" && tar -cvjf $repo_dir.tar.bz2 $repo_dir; done
 
     for clouddir in "${CLOUD_DIR_LIST[@]}"
     do
-        cp *.zip /home/$HOME_USER_NAME/$clouddir/backup/git-store-cloned/
+        cp *.tar.bz2 /home/$HOME_USER_NAME/$clouddir/backup/git-store-cloned/
     done
 popd
 rm -rf /tmp/backup-git-store
