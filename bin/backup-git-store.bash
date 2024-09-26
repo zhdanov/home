5db9173f0aaf30f0ab92836d75e1bc34d535880e
zhdanov <george@jupiter.solutions>
2024-09-26 08:23:12 +0300

    fix(backup): git-store

--- bin/backup-git-store.bash
+++ bin/backup-git-store.bash
@@ -1,5 +1,13 @@
 #!/bin/bash
-pushd /home/$USER/
+
+if [[ $# -ne 1 ]]; then
+    echo "usage:"
+    echo "$0 username"
+    exit 0
+fi
+
+HOME_USER_NAME=$1
+pushd /home/$HOME_USER_NAME/
     zip -r git-store.zip git-store
     mv git-store.zip data-store/gitlab-prod/
 popd
