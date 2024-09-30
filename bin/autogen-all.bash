452e8863a3b43d3448750db0acf1cff3a267673e
zhdanov <george@jupiter.solutions>
2024-09-30 19:45:13 +0300

    feat(jobs): the films generator was improved

--- bin/autogen-all.bash
+++ bin/autogen-all.bash
@@ -6,6 +6,7 @@ pushd /opt/my-opinion/
     git pull origin master
 
     pushd resource/2.4/
-        autogen --vault=../ --out=. --tags=autogen,mind,films,dev,common,backup,monitoring,confidence,jobber --prefix=2.4 --no_tags_is_ok=true
+        autogen --vault=../ --out=. --tags=autogen,mind,dev,common,backup,monitoring,confidence,jobber --prefix=2.4 --no_tags_is_ok=true
+        autogen --vault=/home/zhdanov/vault-store/films/vault/ --out=. --tags=films
     popd
 popd
