81b6cfe614218466ba6580a40429fdc8a83200db
zhdanov <yuriy.zhdanov@gmail.com>
2025-05-09 15:29:35 +0300

    feat(setup): web server packages

--- setup/setup__packages.bash
+++ setup/setup__packages.bash
@@ -4,7 +4,7 @@ sudo apt clean
 sudo apt -y update
 sudo apt -y upgrade
 
-sudo apt -y install vim vifm git gitk openssh-server net-tools curl maim hdparm tree nfs-kernel-server ffmpeg unrar nodejs npm tmux vim htop atop smem fzf ripgrep ncdu httpie gcal goldendict shotwell mpv cryptsetup
+sudo apt -y install vim vifm git gitk openssh-server net-tools curl maim hdparm tree nfs-kernel-server ffmpeg unrar nodejs npm tmux vim htop atop smem fzf ripgrep ncdu httpie gcal goldendict mpv cryptsetup
 sudo apt -y install i3 i3status thunar ttf-mscorefonts-installer kate brightnessctl tcptrack pasystray samba sqlite3 pulseaudio pavucontrol sshfs whois
 sudo apt -y install clang-format cppcheck valgrind
 
@@ -39,13 +39,14 @@ sudo apt -y install chromium-browser
 
 #fi
 
+# web server
+sudo apt -y install nginx php-fpm
+
 # php
-sudo apt -y install php-cli php-dom php-mbstring php-curl php-yaml php-sqlite3 php-zip php-gd php-redis php-mysql
+sudo apt -y install php-cli php-dom php-xml php-mbstring php-curl php-yaml php-sqlite3 php-zip php-gd php-redis php-mysql
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 sudo mv composer.phar /usr/local/bin/composer
 
-# npm
-npm list -g http-server || sudo npm install -g http-server
