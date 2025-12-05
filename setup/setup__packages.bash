ca40e2e82eacd0d750e7e746de7036f4ea22b6e1
zhdanov <yuriy.zhdanov@gmail.com>
2025-12-05 22:09:43 +0300

    ref setup cleaning

--- setup/setup__packages.bash
+++ setup/setup__packages.bash
@@ -41,15 +41,3 @@ sudo apt -y install chromium-browser
     # snap end
 
 #fi
-
-# web server
-sudo apt -y install nginx php-fpm
-
-# php
-sudo apt -y install php-cli php-dom php-xml php-mbstring php-curl php-yaml php-sqlite3 php-zip php-gd php-redis php-mysql
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-sudo mv composer.phar /usr/local/bin/composer
-
