f98e38531f7097a580966d0b75eb844cc731089c
zhdanov <george@jupiter.solutions>
2025-12-07 08:16:25 +0300

    feat(visudo): incus-root sudo password required

--- setup/setup__configure-visudo.bash
+++ setup/setup__configure-visudo.bash
@@ -3,3 +3,7 @@
 if ! sudo grep -q '%sudo ALL=(ALL) NOPASSWD: ALL' /etc/sudoers; then
     echo "%sudo ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
 fi
+
+if ! sudo grep -q '^incus-root ' /etc/sudoers; then
+    echo "incus-root ALL=(ALL) PASSWD: ALL" | sudo tee -a /etc/sudoers
+fi
