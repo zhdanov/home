4f07b6418bc144ab265af3a4bd52ebec298c9d95
zhdanov <george@jupiter.solutions>
2026-05-03 04:12:02 +0300

    feat(setup): chromium as a default browser

--- setup/setup__packages_desktop.bash
+++ setup/setup__packages_desktop.bash
@@ -11,6 +11,24 @@ sudo tee /usr/local/bin/chromium > /dev/null << 'EOF'
 exec flatpak run org.chromium.Chromium "$@"
 EOF
 sudo chmod +x /usr/local/bin/chromium
+mkdir -p /home/$HOME_USER_NAME/.local/share/applications
+sudo tee /home/$HOME_USER_NAME/.local/share/applications/chromium.desktop > /dev/null << 'EOF'
+[Desktop Entry]
+Version=1.0
+Name=Chromium
+GenericName=Web Browser
+Exec=/usr/local/bin/chromium %U
+Terminal=false
+Type=Application
+Categories=Network;WebBrowser;
+MimeType=text/html;text/xml;application/xhtml+xml;x-scheme-handler/http;x-scheme-handler/https;
+StartupNotify=true
+EOF
+chmod +x /home/$HOME_USER_NAME/.local/share/applications/chromium.desktop
+xdg-settings set default-web-browser chromium.desktop
+xdg-mime default chromium.desktop x-scheme-handler/http
+xdg-mime default chromium.desktop x-scheme-handler/https
+xdg-mime default chromium.desktop text/html
 
 # dbeaver
 sudo flatpak install -y flathub io.dbeaver.DBeaverCommunity
@@ -20,7 +38,7 @@ exec flatpak run io.dbeaver.DBeaverCommunity "$@"
 EOF
 sudo chmod +x /usr/local/bin/dbeaver
 
-# dbeaver
+# zoom
 sudo flatpak install -y flathub us.zoom.Zoom
 sudo tee /usr/local/bin/zoom > /dev/null << 'EOF'
 #!/bin/bash
